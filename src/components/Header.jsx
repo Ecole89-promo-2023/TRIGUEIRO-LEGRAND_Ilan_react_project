11345ce8708c7c4a2bd3b85b6f050f0f1f01a301
Mzgus <ilanetgus77@gmail.com>
2025-02-09 21:08:02 +0100

    now trying to sort by language

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -40,7 +40,7 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/cards">Listes des cartes</Nav.Link>
           <NavDropdown title="Langues" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Français</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
